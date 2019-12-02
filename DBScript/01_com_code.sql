cc5cd3a04f62125f1a9a9fbea12af13d68c1d00b
Muskan Nechlani <34474558+muskannechlani@users.noreply.github.com>
2019-12-02 22:32:00 +0530

    Update 01_com_code.sql

--- DBScript/01_com_code.sql
+++ DBScript/01_com_code.sql
@@ -2,11 +2,11 @@
 
 
 CREATE TABLE com_code(
-com_codeGroupId int(11) ,
 com_codeId int(11),
+com_codeGroupId int(11) ,
 com_codeName varchar(100) NOT NULL,
 foreign key(com_codeGroupId) REFERENCES com_codeGroup(com_codeGroupId) ON UPDATE CASCADE ON DELETE CASCADE,
-primary key(com_codeGroupId,com_codeId)
+primary key(com_codeId)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -14,16 +14,9 @@ primary key(com_codeGroupId,com_codeId)
 --
 
 INSERT INTO com_code (com_codeGroupId,com_codeId, com_codeName) VALUES
-(100, 001,'female'),
-(100, 002,'male'),
-(101, 001,'government'),
-(101, 002,'private'),
-(102, 001,'question'),
-(102, 002,'option'),
-(102, 003,'user'),
-(103, 001,'Multi_Select'),
-(103, 002,'Single_Select'),
-(105, 001,'English');
+(100001, 100,'female'),
+(100002, 100,'male');
+
 
 
 --
