53c200cb919f867829c3fb3ab1a4007c6fa7b1f8
Rucha-Nandgirikar <44308162+Rucha-Nandgirikar@users.noreply.github.com>
2019-12-04 16:19:31 +0530

    Add files via upload

---
+++ DBScript/01_qus_question.sql
@@ -0,0 +1,13 @@
+CREATE TABLE 01_qus_question(
+question_ID INT,
+country_id INT NOT NULL,
+domain_codeId INT,
+type_codeId INT,
+PRIMARY KEY(question_id),
+FOREIGN KEY (country_id) REFERENCES com_countries(id),
+FOREIGN KEY(domain_codeId) REFERENCES com_code(com_codeId),
+FOREIGN KEY(type_codeId) REFERENCES com_code(com_codeId)
+)ENGINE=INNODB DEFAULT CHARSET=latin1;
+
+INSERT INTO 01_qus_question
+(question_ID,country_id,domain_codeId,type_codeId) VALUES (1,99,104,102);
